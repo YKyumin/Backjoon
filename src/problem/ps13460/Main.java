373e8990db1e67bf9864c7248d90f117fc1f3a5b
Kyumin <ra88tg@gmail.com>
2024-06-11 13:07:52 +0900

    13460 logic making

--- src/problem/ps13460/Main.java
+++ src/problem/ps13460/Main.java
@@ -1,6 +1,8 @@
 package problem.ps13460;
 
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Main {
 	final static private int[] dx = { 1, 0, -1, 0 };
@@ -9,6 +11,7 @@ public class Main {
 	static boolean[][][][] visited;
 	static int n;
 	static int m;
+	static Queue<int[]> queue = new ConcurrentLinkedQueue<int[]>();
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -29,9 +32,11 @@ public class Main {
 				if (board[i][j] == 'R') {
 					rPos[0] = i;
 					rPos[1] = j;
+					board[i][j] = '.';
 				} else if (board[i][j] == 'B') {
 					bPos[0] = i;
 					bPos[1] = j;
+					board[i][j] = '.';
 				} else if (board[i][j] == '.' || board[i][j] == '#' || board[i][j] == 'O') {
 
 				} else {
@@ -51,15 +56,61 @@ public class Main {
 
 	private static int executeBFS(int i, int j, int k, int l) {
 		visited[i][j][k][l] = true;
+		queue.add(new int[] { i, j, k, l });
 
-		for (int a = 0; a < 4; a++) {
-			int rx = i;
-			int ry = j;
-			int bx = k;
-			int by = l;
-			boolean isBlueOut = false;
+		while (queue.isEmpty()) {
+			int[] curPos = queue.poll();
 
-			//TODO move and recursive logic
+			for (int a = 0; a < 4; a++) {
+				int rx = curPos[0];
+				int ry = curPos[1];
+				int bx = curPos[2];
+				int by = curPos[3];
+				boolean isBlueOut = false;
+
+				// TODO move and logic
+				// x move
+				boolean rxFlag = false;
+				boolean bxFlag = false;
+				while (dx[a] != 0) {
+					// red
+					if (rx + dx[a] > -1 && rx + dx[a] < n) { // inmap check
+						if (board[rx + dx[a]][ry] == '.') {
+							// bx check
+							if (rx + dx[a] != bx && ry != rx) {
+								rx = rx + dx[a];
+							} else {
+								if (board[bx + dx[a]][by] == '.') {
+									rx = rx + dx[a];
+								} else {
+									rxFlag = true; // red stop
+								}
+							}
+
+						} else if (board[rx + dx[a]][ry] == 'O') {
+							// TODO
+						} else {
+							rxFlag = true; // red stop
+						}
+					} else {
+						rxFlag = true;
+					}
+
+					// blue
+					if (bx + dx[a] > -1 && bx + dx[a] < n) {
+						if (board[bx + dx[a]][by] == '.') {
+							// rx check
+						} else if (board[bx + dx[a]][by] == 'O') {
+							// TODO
+						} else {
+							bxFlag = true;
+						}
+					} else {
+						bxFlag = true;
+					}
+				}
+
+			}
 
 		}
 
